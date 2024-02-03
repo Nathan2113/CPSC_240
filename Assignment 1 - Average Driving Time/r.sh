838fa1e8578bea080600c3195e6b55e625f94335
Nathan2113 <warnernathan2001@gmail.com>
2024-02-02 22:33:04 -0800

    Update r.sh

--- Assignment 1 - Average Driving Time/r.sh
+++ Assignment 1 - Average Driving Time/r.sh
@@ -1,18 +1,12 @@
 #/bin/bash
-
 #Program name "Driving Time"
 #Author: Nathan Warner
 #This file is the script file that accompanies the "Driving Time" program.
-
 #Delete some un-needed files
 rm *.o
 rm *.out
-
 nasm -f elf64 -l average.lis -o average.o average.asm
-
 gcc  -m64 -Wall -no-pie -o driving_time.o -std=c2x -c driving_time.c
-
 gcc -m64 -no-pie -o driving_time.out average.o driving_time.o -std=c2x -Wall -z noexecstack
-
 chmod +x driving_time.out
 ./driving_time.out
