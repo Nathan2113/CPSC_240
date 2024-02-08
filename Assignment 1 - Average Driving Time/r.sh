7edd0e2306915c8f77e05d0bf161bcb66e54bbce
Nathan2113 <111931927+Nathan2113@users.noreply.github.com>
2024-02-07 21:53:51 -0800

    fixed issue?

--- Assignment 1 - Average Driving Time/r.sh
+++ Assignment 1 - Average Driving Time/r.sh
@@ -3,7 +3,7 @@
 #Program name "Driving Time"
 #Author: Nathan Warner
 #CWID: 884688250
-#This file is the script file that accompanies the "Driving Time" program.
+#This file is the script file that accompanies the "Driving Time" program
 #Delete some un-needed files
 
 rm *.o
@@ -11,7 +11,7 @@ rm *.out
 
 nasm -f elf64 -l average.lis -o average.o average.asm
 
-gcc  -m64 -Wall -no-pie -o driving_time.o -std=c2x -c driving_time.c
+gcc -m64 -Wall -no-pie -o driving_time.o -std=c2x -c driving_time.c
 
 gcc -m64 -no-pie -o driving_time.out average.o driving_time.o -std=c2x -Wall -z noexecstack
 
