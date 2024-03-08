80a6ded57242e33eca33e7ad93c55d58a0d5c7e4
Nathan2113 <111931927+Nathan2113@users.noreply.github.com>
2024-03-07 20:50:58 -0800

    Final draft 1

--- Assignment 3 - Arrays/compute_mean.asm
+++ Assignment 3 - Arrays/compute_mean.asm
@@ -1,16 +1,23 @@
 ;****************************************************************************************************************************
-;Program name: "Amazing Triangles" - This program will take first welcome the user the the program, and then output the     *
-;system clock (tics) to the console. After this initial output for the user, the program will then prompt the user for their*
-;full name, as well as their title (i.e. Dean, Vice-president, etc.). Once the user has entered their name and title, the   *
-;program will tell them good morning, and that this program will take care of their triangles. After, the program will      *
-;prompt the user for the sides of the triangle and its angle (this program solves SAS triangles). If the user inputs an     *
-;invalid input (negative number, non-float number, or an input that is not a number such as 2.2.3+A), the program will      *
-;let the user know that their input is invalid and will then prompt them for another input. After 3 valid inputs are        *
-;entered (2 sides and 1 angle), the program will output a thank you message/confirmation of the user's inputted values.     *
-;Now that the program has 3 valid inputs, it will use the formula for solving SAS triangles to find the third side, and     *
-;will output said answer, as well as letting the user know that the length of the third side will be sent to the driver.    *
-;Before this value is sent, the program will output the new system clock (tics). Once back in the driver, it will let the   *
-;user know that it has received the value of the third side, and that a zero will be sent to the operating system.          *                                                                                                      
+;Program name: "Arrays" - This program will first welcome the user to the program, as well as outputting its developer.     *
+;After this initial message, the program will let the user know the directions of the program, which is as follows:         *
+;                                                                                                                           *
+;"This program will manage your arrays of 64-bit floats                                                                     *
+;For the array enter a sequence of 64-bit floats separated by white space.                                                  *
+;After the last input press enter followed by Control+D:"                                                                   *
+;                                                                                                                           *
+;The program will then take in user input, validating each input to make sure they are entering valid float numbers, and    *
+;this process is done through the input_array.asm file, using isfloat.asm to validate their inputs. If the user inputs an   *
+;invalid input, the program will let them know with the following message:                                                  *
+;                                                                                                                           *
+;"The last input was invalid and not entered into the array.""                                                              *
+;                                                                                                                           *
+;Once the array has been fully entered, the program will output the entire array to the screen, which is done in the        *
+;output_array.c file using the C language. Once the array has been output, the program will then compute the mean of the    *
+;array using compute_mean.asm, and will then use the mean it found to compute the variance using compute_variance.cpp,      *
+;which uses C++. Once the variance has been found, the program will output the variance to the screen for the user, and     *
+;will then send the variance to main.c, where the program will let the user know that the variance will be kept for         *
+;future use, and that a 0 will be sent to the operating system.                                                             *
 ;                                                                                                                           *
 ;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
 ;version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful,   *
@@ -28,30 +35,25 @@
 ;  Author email: nwarner4@csu.fullerton.edu
 ;
 ;Program information
-;  Program name: Amazing Triangles
-;  Programming languages: One module in C, one in X86, and one in bash.
-;  Date program began: 2024-Feb-11
-;  Date of last update: 2024-Feb-19
-;  Files in this program: driving_time.c, average.asm, r.sh.
+;  Program name: Arrays
+;  Programming languages: Two modules in C, four modules in x86_64, one module in C++, and one module in bash
+;  Date program began: 2024-Mar-3
+;  Date of last update: 2024-Mar-7
+;  Files in this program: main.c, manager.asm, r.sh, output_array.c, compute_mean.asm, compute_variance.cpp, input_array.asm, isfloat.asm
 ;  Testing: Alpha testing completed.  All functions are correct.
 ;  Status: Ready for release to customers
 ;
 ;Purpose
-;  This program will take in two sides and an angle of a triangle, and will output the length of the
-;   thrd side to the console, as well as sending the value to the driver.
+;  The program will take in an array of valid floating point numbers from the user, find the mean of the array, 
+;       and find the variance, which it will output to the screen and send to main.c
 ;
 ;This file:
-;  File name: triangle.asm
+;  File name: compute_mean.asm
 ;  Language: X86-64
 ;  Max page width: 124 columns
-;  Assemble (standard): nasm -l average.lis -o average.o average.asm
-;  Assemble (debug): nasm -g dwarf -l average.lis -o average.o average.asm
+;  Assemble (standard): nasm -f elf64 -l compute_mean.lis -o compute_mean.o compute_mean.asm
 ;  Optimal print specification: Landscape, 7 points, monospace, 8½x11 paper
-;  Prototype of this function: double triangle_SAS();
-; 
-;
-;
-;
+;  Prototype of this function: double compute_mean(double *array, int size);
 ;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
 
 ;Declaration section.  The section has no name other than "Declaration section".  Declare here everything that does
@@ -63,14 +65,9 @@ extern printf
 
 segment .data
 ;This section (or segment) is for declaring initialized arrays
-    ; string_format db "%s", 0
-    ; user_invalid_input db "The last input was invalid and not entered into the array.", 10, 0
     return_value db 10, "The return value is: %1.6lf", 10, 0
     format db "%lf", 0
     return_value_reg db 10, "The return value is: %lu", 10, 0
-
-    ;TESTING
-    three dq 6.0
 
 segment .bss
 ;This section (or segment) is for declaring empty arrays
@@ -112,13 +109,7 @@ compute_mean:
     mov r15, 0   ;r15 is the current index
 
     movsd xmm13, [r13]
-    movsd xmm14, [r15]
-
-    mov rax, 1
-    mov rdi, return_value
-    mov rsi, format
-    movsd xmm0, xmm13
-    call printf
+    cvtsi2sd xmm14, r14 ;Converting size of the array to float and storing value in xmm14
 
 
 begin:
@@ -132,17 +123,8 @@ begin:
     
 quit:
     ;Takes the sum of the array and divides it by the number of elements (mean)
-    ; divsd xmm12, qword [three]
     divsd xmm12, xmm14
     
-
-
-    mov rax, 1
-    mov rdi, return_value
-    mov rsi, format
-    movsd xmm0, xmm12
-    call printf
-
 
     push qword 0
     movsd [rsp], xmm12
--- Assignment 3 - Arrays/r.sh
+++ Assignment 3 - Arrays/r.sh
@@ -1,56 +1,63 @@
-#****************************************************************************************************************************
-#Program name: "Amazing Triangles" - This program will take first welcome the user the the program, and then output the     *
-#system clock (tics) to the console. After this initial output for the user, the program will then prompt the user for their*
-#full name, as well as their title (i.e. Dean, Vice-president, etc.). Once the user has entered their name and title, the   *
-#program will tell them good morning, and that this program will take care of their triangles. After, the program will      *
-#prompt the user for the sides of the triangle and its angle (this program solves SAS triangles). If the user inputs an     *
-#invalid input (negative number, non-float number, or an input that is not a number such as 2.2.3+A), the program will      *
-#let the user know that their input is invalid and will then prompt them for another input. After 3 valid inputs are        *
-#entered (2 sides and 1 angle), the program will output a thank you message/confirmation of the user's inputted values.     *
-#Now that the program has 3 valid inputs, it will use the formula for solving SAS triangles to find the third side, and     *
-#will output said answer, as well as letting the user know that the length of the third side will be sent to the driver.    *
-#Before this value is sent, the program will output the new system clock (tics). Once back in the driver, it will let the   *
-#user know that it has received the value of the third side, and that a zero will be sent to the operating system.          *                                                                                                      
-#                                                                                                                           *
-#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-#version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful,   *
-#but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See   *
-#the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:             *
-#<https://www.gnu.org/licenses/>.                                                                                           *
-#****************************************************************************************************************************
-
+# Program name: "Arrays" - This program will first welcome the user to the program, as well as outputting its developer.     *
+# After this initial message, the program will let the user know the directions of the program, which is as follows:         *
+#                                                                                                                            *
+# "This program will manage your arrays of 64-bit floats                                                                     *
+# For the array enter a sequence of 64-bit floats separated by white space.                                                  *
+# After the last input press enter followed by Control+D:"                                                                   *
+#                                                                                                                            *
+# The program will then take in user input, validating each input to make sure they are entering valid float numbers, and    *
+# this process is done through the input_array.asm file, using isfloat.asm to validate their inputs. If the user inputs an   *
+# invalid input, the program will let them know with the following message:                                                  *
+#                                                                                                                            *
+# "The last input was invalid and not entered into the array.""                                                              *
+#                                                                                                                            *
+# Once the array has been fully entered, the program will output the entire array to the screen, which is done in the        *
+# output_array.c file using the C language. Once the array has been output, the program will then compute the mean of the    *
+# array using compute_mean.asm, and will then use the mean it found to compute the variance using compute_variance.cpp,      *
+# which uses C++. Once the variance has been found, the program will output the variance to the screen for the user, and     *
+# will then send the variance to main.c, where the program will let the user know that the variance will be kept for         *
+# future use, and that a 0 will be sent to the operating system.                                                             *
+#                                                                                                                            *
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
+# version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful,   *
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See   *
+# the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:             *
+# <https://www.gnu.org/licenses/>.                                                                                           *
 
 
 
 #========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
-#  Author information
-#  Author name: Nathan Warner
-#  Author email: nwarner4@csu.fullerton.edu
-#
-#Program information
-#  Program name: Amazing Triangles
-#  Programming languages: One module in C, one in X86, and one in bash.
-#  Date program began: 2024-Feb-11
-#  Date of last update: 2024-Feb-19
-#  Files in this program: driving_time.c, average.asm, r.sh.
-#  Testing: Alpha testing completed.  All functions are correct.
-#  Status: Ready for release to customers
-#
-#Purpose
-#  This program will take in two sides and an angle of a triangle, and will output the length of the
-#   thrd side to the console, as well as sending the value to the driver.
-#
-#This file:
-#  File name: r.sh
-#  Language: bash
-#  Max page width: 124 columns
-#  Optimal print specification: Landscape, 7 points, monospace, 8½x11 paper
+# Author information
+#   Author name: Nathan Warner
+#   Author email: nwarner4@csu.fullerton.edu
+
+# Program information
+#   Program name: Arrays
+#   Programming languages: Two modules in C, four modules in x86_64, one module in C++, and one module in bash
+#   Date program began: 2024-Mar-3
+#   Date of last update: 2024-Mar-7
+#   Files in this program: main.c, manager.asm, r.sh, output_array.c, compute_mean.asm, compute_variance.cpp, input_array.asm, isfloat.asm
+#   Testing: Alpha testing completed.  All functions are correct.
+#   Status: Ready for release to customers
+
+# Purpose
+#   The program will take in an array of valid floating point numbers from the user, find the mean of the array, 
+#        and find the variance, which it will output to the screen and send to main.c
+
+# This file:
+#   File name: r.sh
+#   Language: Bash
+#   Max page width: 124 columns
+#   Optimal print specification: Landscape, 7 points, monospace, 8½x11 paper
+#   How to call r.sh: ./r.sh
+#========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+
 
 #/bin/bash
 
-#Program name "Amazing Triangles"
+#Program name "Arrays"
 #Author: Nathan Warner
-#This file is the script file that accompanies the "Amazing Triangles" program.
+#This file is the script file that accompanies the "Arrays" program.
 
 #Delete some un-needed files
 rm *.o
